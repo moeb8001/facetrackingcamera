53e3c3d4cc726c4369aaf77fc950b6417f7bc3ea
moeb8001 <112695184+moeb8001@users.noreply.github.com>
2022-09-22 21:43:32 +0200

    Add files via upload

---
+++ Source Code/face_detect.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+img=cv.imread("8ppl.jpg")
+cv.imshow("Person",img)
+
+gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+cv.imshow("Gray Peson",gray)
+
+haar_cascade=cv.CascadeClassifier("haar_face.xml")
+
+faces_rect=haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
+
+
+print(f"Number of faces found = {len(faces_rect) }")
+
+list_of_coordinates=[]
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y),(x+w,y+h),(0,255,0),thickness=2)
+    list_of_coordinates.append((x,y,w,h))
+
+print(list_of_coordinates)
+cv.imshow('Detected Faces',img)
+
+cv.waitKey(0)
